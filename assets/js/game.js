a2d36a47e2175f034fe094d4a35e628da36187af
Swan Pham <accounting@nailsrus.ca>
2021-03-11 20:19:03 -0500

    index.html

---
+++ assets/js/game.js
@@ -0,0 +1,9 @@
+
+var playerName = window.prompt("What is your robot's name?");
+window.alert("This is an alert! JavaScript is running!");
+// this creates a function named "fight"
+
+function fight() {
+    window.alert("The fight has begun!");
+  }
+  fight();  
