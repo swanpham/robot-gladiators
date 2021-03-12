1eaf475bf1f8a46705adf80d594d363cb1968c62
Swan Pham <accounting@nailsrus.ca>
2021-03-11 23:52:26 -0500

    index.html

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,9 +1,30 @@
-
 var playerName = window.prompt("What is your robot's name?");
+
+var playerName = "Swan The Robot";
+console.log(playerName);
+console.log("This logs a string, good for leaving yourself a message");
+// this will do math and log 20
+console.log(10 + 10);
+// what is this?
+console.log("Our robot's name is " + playerName);
+// Note the lack of quotation marks around playerName
+// What is this?
+
 window.alert("This is an alert! JavaScript is running!");
+// Tony the Robot is ready for battle!
+console.log("Swan the Robot" + " is ready for battle!");
+
+// "Tony the Robot is ready for battle!"
+console.log(playerName + " is ready for battle!");
+
+// "Your robot, Tony the Robot, has won!
+console.log("Your robot, " + playerName + ", has won!");
+
+var name = "Swan Pham";
+console.log(name);
 // this creates a function named "fight"
 
 function fight() {
-    window.alert("The fight has begun!");
-  }
-  fight();  
+  window.alert("The fight has begun!");
+}
+// fight();
